e8fb9720831c991d16af4027b9471d117c433fd9
Nicolate Makubika <n.makubika5583@student.leedsbeckett.ac.uk>
2023-11-09 17:10:30 +0000

    done

---
+++ main.py
@@ -0,0 +1,8 @@
+# Prompt the user to enter their name
+user_name = input("Hello. What is your name? ")
+
+# Check if the user entered a name
+if user_name:
+    print(f"Well met, {user_name}! It is good to meet you!")
+else:
+    print("Well met, Mysterious Stranger! How shall we learn your name?")
